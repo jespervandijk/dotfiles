f3bcdd0578029107c308235df92614f7ac8dde8b
jespervandijk <jespervandijk@icloud.com>
2025-04-19 11:07:22 +0200

    add Tools folder to PATH

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,3 @@
 (@(& 'C:/Users/Jesper/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe' init pwsh --config='C:\Users\Jesper\AppData\Local\Programs\oh-my-posh\themes\tokyonight_storm.omp.json' --print) -join "`n") | Invoke-Expression
+# Add the tools folder to your PATH
+$env:PATH += ";C:\Tools"
