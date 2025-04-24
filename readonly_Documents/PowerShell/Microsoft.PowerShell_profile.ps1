18ccf4354bf54e5193c5b035b21fcffbc05e92ae
Jesper van Dijk <90760181+jespervandijk@users.noreply.github.com>
2025-04-24 10:23:10 +0200

    work settings

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,8 @@
 (@(& 'C:/Users/Jesper/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe' init pwsh --config='C:\Users\Jesper\AppData\Local\Programs\oh-my-posh\themes\tokyonight_storm.omp.json' --print) -join "`n") | Invoke-Expression
 # Add the tools folder to your PATH
 $env:PATH += ";C:\Tools"
+
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\tokyonight_storm.omp.json" | Invoke-Expression
+# function goto-haas-be { Set-Location C:\repos\Hypotheek\services\Hypotheekdossier\src\Abn.Hypotheekdossiers.Api }
+# function goto-haas-fe { Set-Location C:\repos\Hypotheek\apps\haas-ui }
+# function goto-haas-tools { Set-Location C:\repos\Hypotheek\.tools }
