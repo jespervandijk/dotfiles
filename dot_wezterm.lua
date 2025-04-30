fe202df6a67a896efeafb55334b2618c16c9b782
jespervandijk <j.vandijk@tjip.com>
2025-04-30 16:38:06 +0200

    wezterm shortcuts

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -9,6 +9,23 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = 'Tokyo Night'
 config.default_prog = {"C:\\Program Files\\PowerShell\\7\\pwsh.exe"}
+config.keys = {
+    {
+        key = "n",
+        mods = "CTRL",
+        action = wezterm.action.ActivateTabRelative(1),
+    },
+    {
+        key = "n",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.ActivateTabRelative(-1),
+    },
+{
+        key = "c",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+    }
+}
 
 -- and finally, return the configuration to wezterm
 return config
