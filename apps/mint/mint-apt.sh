7d1d0a68d9c386148648d1936773517c372b1a9f
jespervandijk <jespervandijk@icloud.com>
2025-11-28 16:58:20 +0100

    add razer to mint

--- apps/mint/mint-apt.sh
+++ apps/mint/mint-apt.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+sudo apt install software-properties-gtk
+
 # Add WezTerm APT repository
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
+
+# Add OpenRazer APT repository
+sudo add-apt-repository ppa:openrazer/stable
+
+# Add Polychromatic APT repository
+sudo add-apt-repository ppa:polychromatic/stable
 
 sudo apt update
 
 sudo apt install -y \
     git \
     wezterm \
-    rofi
+    rofi \
+    openrazer-meta \
+    polychromatic
 
 echo "Apt packages installed!"
