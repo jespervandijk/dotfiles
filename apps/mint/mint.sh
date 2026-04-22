d7c24137c3b1d8a2e331bd3f4326b3f2cf3db071
jespervandijk <j.vandijk@tjip.com>
2026-04-22 11:15:21 +0200

    more buf

--- apps/mint/mint.sh
+++ apps/mint/mint.sh
@@ -159,6 +159,7 @@ deb_get_packages() {
 
 go_installs(){
     go install golang.org/x/tools/gopls@latest
+    go install github.com/bufbuild/buf/cmd/buf@latest
     go get -tool google.golang.org/protobuf/cmd/protoc-gen-go@latest
     go get -tool connectrpc.com/connect/cmd/protoc-gen-connect-go@latest
 }
--- apps/wsl/ubuntu-wsl.sh
+++ apps/wsl/ubuntu-wsl.sh
@@ -92,7 +92,8 @@ install_scripts() {
 
 go_installs(){
     go install golang.org/x/tools/gopls@latest
-    go get -tool google.golang.org/protobuf/cmd/protoc-gen-go@latest
+    go install github.com/bufbuild/buf/cmd/buf@latest
+    go get -tool TijdelijkeErfpachtgoogle.golang.org/protobuf/cmd/protoc-gen-go@latest
     go get -tool connectrpc.com/connect/cmd/protoc-gen-connect-go@latest
 }
 
